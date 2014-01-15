78bdbdfc0c02845c2bdd0fba1e0595abb1debba5
Maximilian Konzack <mkonzack@acm.org>
2014-01-15 15:43:27 +0100

    Simple API for converting between GPX and pandas

---
+++ gpxpandas/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'max'
+
+
+def read_gpx(gpx_file_name):
+    # TODO read gpx file into pandas
+    pass
+
+
+def write_gpx(pd_data_frame):
+    # TODO write gpx file from pandas
+    pass
