e704789e387a5e542673c2a609e9981527e3ad1d
Maximilian Konzack <mkonzack@acm.org>
2014-01-16 16:26:27 +0100

    reading into pandas also from gpx object itself

--- gpxpandas/__init__.py
+++ gpxpandas/__init__.py
@@ -3,10 +3,14 @@ __author__ = 'max'
 import gpxpandas.gpxreader as gpx_reader
 
 
-def read_gpx(gpx_file_name):
+def read_gpx_file(gpx_file_name):
     with open(gpx_file_name, 'r') as gpx_file:
         gpx = gpx_reader.parse_gpx(gpx_file)
-        return gpx_reader.pandas_data_frame_for_gpx(gpx)
+        return read_gpx(gpx)
+
+
+def read_gpx(gpx):
+    return gpx_reader.pandas_data_frame_for_gpx(gpx)
 
 
 def write_gpx(pd_data_frame):
